89783a1bbf478e08568042737f4667150f75f932
vbouault <vbouault@hotmail.fr>
2020-08-25 18:14:51 +0200

    favicon

--- src/projects.js
+++ src/projects.js
@@ -11,7 +11,7 @@ const projects = [
   {
     name: 'Valiris Résidence',
     projectInfo: 'Projet réalisé à la Wild code school pour la société Valiris Résidence',
-    siteInfo: 'Front office pour mettre en avant et voir les disponibilités des appartements de la société et le back office permetant de gérer les réservations, les appartements et les contacts',
+    siteInfo: 'Front office pour mettre en avant et voir les disponibilitées des appartements de la sociétée et le back office permetant de gérer les réservations, les appartements et les contacts',
     technos: ['Html','Css','React','Node','Express','MySql'],
     image: 'valiris.png',
     link: 'https://cards-battle-of-heroes.netlify.app',
