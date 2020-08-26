7ef79232228f113f8cd62cb72ba22e3870eac394
vbouault <vbouault@hotmail.fr>
2020-08-26 16:40:49 +0200

    add ordoli

--- src/projects.js
+++ src/projects.js
@@ -18,22 +18,22 @@ const projects = [
     link2: 'https://cards-battle-of-heroes.netlify.app'
   },
   {
-    name: 'Cards battle of heroes',
-    projectInfo: 'Projet d\'études réalisé à la Wild code school',
-    siteInfo: 'Jeux de cartes stratégique',
-    technos: ['Html','Css','React'],
-    image: 'CBOH.png',
-    link: 'https://cards-battle-of-heroes.netlify.app',
+    name: 'Ordoli',
+    projectInfo: 'Hackaton de 48h en partenariat avec Doctolib',
+    siteInfo: 'Dématérialisation d\'ordonnances avec un suivi pour le patient',
+    technos: ['Html','Css','React','firebase'],
+    image: 'ordoli.png',
+    link: 'https://ordoli.vbserveur.duckdns.org/',
     link2: null
   },
   {
-    name: 'Valiris Résidence',
+    name: 'Shifumi',
     projectInfo: 'Projet réalisé à la Wild code school pour la société Valiris Résidence',
     siteInfo: 'Front office pour mettre en avant et voir les disponibilités des appartements de la société et le back office permetant de gérer les réservations, les appartements et les contacts',
     technos: ['Html','Css','React','Node','Express','MySql'],
     image: 'valiris.png',
     link: 'https://cards-battle-of-heroes.netlify.app',
-    link2: 'https://cards-battle-of-heroes.netlify.app'
+    link2: null
   }
 ]
 
